f04a6333f1818db622680aad256575c22923e06e
René Fritze <rene.fritze@wwu.de>
2022-11-15 09:48:23 +0100

    add a simpler test compile, skip iostream one on ci

--- test/test_executable.py
+++ test/test_executable.py
@@ -10,7 +10,7 @@ import pytest
 def ensure_tidy_from_wheel(monkeypatch):
     """test the installed clang_tidy package, not the local one"""
     this_dir = Path(__file__).resolve().absolute().parent
-    for pd in (this_dir, this_dir / '..'):
+    for pd in (this_dir, this_dir / ".."):
         try:
             new_path = sys.path.remove(pd)
             monkeypatch.setattr(sys, "path", new_path)
@@ -18,20 +18,35 @@ def ensure_tidy_from_wheel(monkeypatch):
             pass
     monkeypatch.delitem(sys.modules, "clang_tidy", raising=False)
 
+
 def test_executable_file():
     import clang_tidy
+
     exe = clang_tidy._get_executable("clang-tidy")
     assert os.path.exists(exe)
     assert os.access(exe, os.X_OK)
 
 
-def test_include_iostream():
+def _test_code(code: str):
     import clang_tidy
+
     fd, compilation_unit = tempfile.mkstemp(suffix=".cpp")
     os.close(fd)
     with open(compilation_unit, "w") as ostr:
-        ostr.write("#include <iostream>\n")
+        ostr.write(code)
     try:
         assert clang_tidy._run("clang-tidy", "--extra-arg=-v", compilation_unit) == 0
     finally:
         os.remove(compilation_unit)
+
+
+@pytest.mark.skipif(
+    os.environ.get("CI", None) and "linux" in sys.platform,
+    reason="https://github.com/ssciwr/clang-tidy-wheel/issues/30",
+)
+def test_include_iostream():
+    _test_code("#include <iostream>\n")
+
+
+def test_main():
+    _test_code("int main() { return 0;}\n")
